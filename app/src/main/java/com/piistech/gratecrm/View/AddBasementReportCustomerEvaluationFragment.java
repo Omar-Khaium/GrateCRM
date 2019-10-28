d1edc9d18c06af262cdd1acac140eacf00905579
Omar Khaium Chowdhury <aunto.aiub@gmail.com>
2019-10-28 11:51:07 +0600

    Customer basement evaluation added

---
+++ app/src/main/java/com/piistech/gratecrm/View/AddBasementReportCustomerEvaluationFragment.java
@@ -0,0 +1,31 @@
+package com.piistech.gratecrm.View;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.piistech.gratecrm.R;
+
+public class AddBasementReportCustomerEvaluationFragment extends Fragment{
+
+    View fragmentView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fragmentView =  inflater.inflate(R.layout.step_add_basement_visual_basement_inspection, container, false);
+        return fragmentView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        fragmentView = null;
+        System.gc();
+    }
+}
